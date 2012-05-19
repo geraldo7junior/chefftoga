5cdab5cb28c8572683e72008dfcbcdefe153497a
mauriokk <mauriokk@c4bcd6d0-dfc6-b098-e016-7486c9956268>
2012-05-19 14:13:15 +0000

    execução

---
+++ Project/Ru/EntradaRuGerenciar.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ru
+{
+    public partial class fEntradaRuGerenciar : Form
+    {
+        public fEntradaRuGerenciar()
+        {
+            InitializeComponent();
+        }
+
+        private void msMenuCadastroNovo_Click(object sender, EventArgs e)
+        {
+            fCadastroNovo _fCad;
+            _fCad = new fCadastroNovo();
+            _fCad.Show();
+         
+            //fIndex _fLog;
+            //_fLog = new fIndex();
+            //_fLog.Show();
+            //Hide();
+
+        }
+
+        private void sairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja realmente encerrar a sessão?", "Sair", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Close();
+                fTelaLogin _tela;
+                _tela = new fTelaLogin();
+                _tela.Show();
+
+            }
+        }
+
+        private void msMenu_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        public void msMenuCadastroAlterar_Click(object sender, EventArgs e)
+        {
+            fAlterar _fAlt;
+            _fAlt = new fAlterar();
+            Utilidades.ControleDeTela = "alterar";
+            _fAlt.Show();
+            
+        }
+
+        public void msMenuCadastroVisualizar_Click(object sender, EventArgs e)
+        {
+            fAlterar _fAlt;
+            _fAlt = new fAlterar();
+            Utilidades.ControleDeTela = "autorizarporcpf";
+            _fAlt.Show();
+        }
+
+        public void msMenuCadastroExcluir_Click(object sender, EventArgs e)
+        {
+            fAlterar _fAlt;
+            _fAlt = new fAlterar();
+            Utilidades.ControleDeTela = "excluir";
+            _fAlt.Show();
+        }
+
+        private void fCadastroGerenciar_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
